9a57d5fa20f0b24afe03183d7f059e935a5dd47d
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-01-12 12:16:02 +0000

    random number generator added

--- script.js
+++ script.js
@@ -7,3 +7,20 @@
 //if boh player and dealer sat && are under 21; compare scores. Higher score wins
 // winning condition: to win - score === 21 (or closest to 21)
 //losing condition; score over 21
+
+let playerScore = 0;
+let dealerScore = 0;
+const cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const randomNumber = [
+  4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+];
+
+
+function firstPickPlayer(min, max) {
+    min = Math.ceil(4);
+    max = Math.floor(21);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+  
+
+console.log(firstPickPlayer());
