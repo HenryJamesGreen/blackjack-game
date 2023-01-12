1567b15e97927134d3f37b0241ce992a3fdd5920
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-01-12 11:35:08 +0000

    added files and pseudocode

---
+++ script.js
@@ -0,0 +1,9 @@
+//player score start = 0
+//dealer score = 0
+//function that gives a random number between 4-21 to player
+//function that gives random number between 2-11 to dealer
+//function to play the game - player gets a turn - player has a score - decides to stick or hit; if hit, deal random number to player - add their random number to their score ;repeat until player decides to stick or goes over or === 21.
+//repeat with dealer ; if dealer is < 17 add number; if dealer is => 17 - stay and run comparison;
+//if boh player and dealer sat && are under 21; compare scores. Higher score wins
+// winning condition: to win - score === 21 (or closest to 21)
+//losing condition; score over 21
